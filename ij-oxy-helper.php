75920123e7da4fbb112a30efe4e05937c598606a
Ivan Juras <ijuras@gmail.com>
2019-07-18 10:03:14 +0200

    Add check if Oxygen exists

--- ij-oxy-helper.php
+++ ij-oxy-helper.php
@@ -12,6 +12,21 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
+add_action( 'plugins_loaded', 'ijoxy_check_if_oxygen_exists');
+function check_if_oxygen_exists() {
+	if (! class_exists('CT_Component') ) {
+		add_action( 'admin_notices', 'ijoxy_no_oxygen_error_notice' );
+		function ijoxy_no_oxygen_error_notice() {
+			echo '
+				<div class="error notice">
+						<p>' . _e( 'There has been an error. Bummer!', 'my_plugin_textdomain' ) . '</p>
+				</div>
+			';
+		}
+	}
+}
+
+
 require_once 'backend/disable.php';
 require_once 'backend/dequeue.php';
 require_once 'backend/vendor-tweaks.php';
